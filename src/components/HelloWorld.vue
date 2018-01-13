7f76306f22cfc86105f922270039c52f44445fad
Stewart Palmer <Stewart.Palmer@gmail.com>
2018-01-13 10:54:25 +0000

    Added Node JS server for Heroku and added Express to packages

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,34 @@
 
 
 <template>
+<div class="slider_container"> 
  <slick ref="slick" :options="slickOptions">
- <div class="slider_element slider_one"> 
-   <div class="slider_inner slider_inner--black"> 
-     <h1 class='title-block__title'>About Test</h1>
+ <div class="slider_element"> 
+ <div class="slider_inner slider_one">
+   <div class="slider_content"> 
+     <h1>About Test</h1>
  <h2 class='title-block__subtitle'>Find out more about the film...</h2>
+  </div>
  </div>
  
  </div>
   <div class="slider_element slider_two">
-   <div class="slider_inner"> 
+   <div class="slider_content"> 
      <h1>The Team</h1>
  <h2>Find out more about the cast and crew...</h2>
  </div>
 
   </div>
   <div class="slider_element slider_three">
-       <div class="slider_inner"> 
+       <div class="slider_content"> 
      <h1>About Test</h1>
  <h2>Find out more about the film...</h2>
  </div>
     </div>  
+    </slick>
+    </div>
  
-</slick>
+
 
 </template>
 
@@ -38,16 +43,11 @@ export default {
         slidesToShow: 1.5,
         slidesToScroll: 1,
         infinite: false,
-        arrows: true,
-        
-
-
-        // Any other options that can be got from plugin documentation
+        arrows: true
       }
     };
   },
 
-  // All slick methods can be used too, example here
   methods: {
     next() {
       this.$refs.slick.next();
@@ -58,7 +58,6 @@ export default {
     },
 
     reInit() {
-      // Helpful if you have to deal with v-for to update dynamic lists
       this.$nextTick(() => {
         this.$refs.slick.reSlick();
       });
@@ -73,25 +72,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.slider_element {
-  height: 100vh;
-}
 
-.slider_one {
-  background-image: url(../assets/POSTER_v3.jpg);
-  background-position: center;
-  background-size: cover;
-}
-
-.slider_two {
-  background-image: url(../assets/IMG_9030.jpg);
-  background-position: center;
-  background-size: cover;
-}
-
-.slider_three {
-  background-image: url(../assets/tank.jpeg);
-  background-position: center;
-  background-size: cover;
-}
 </style>
